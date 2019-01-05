5e5dd4bb1495a5386e7fac67c87b2a24610ee795
daxiaojiudai <937441420@qq.com>
2019-01-05 11:00:46 +0800

    第二天代码完成-step10(数据可视化初步引入)

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,6 +1,41 @@
 $(document).ajaxStart(function () {
     NProgress.start();
 });
-$(document).ajaxStop(function () {
-    NProgress.done();
+$(document).ajaxStop(function() {
+    // 模拟网络延迟
+    setTimeout(function() {
+      // 结束进度条
+      NProgress.done();
+    }, 500);
 });
+
+$(function () {
+    
+    $('.lt_aside .category').on("click", function () {
+        $(this).next().stop().slideToggle();
+    });
+
+    $('.icon-menu').on("click", function () {
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+    });
+
+    $('.icon-logout').on('click', function () {
+        $('#logoutModal').modal("show");
+    });
+
+    $('#logoutBtn').on('click', function () {
+        $.ajax({
+            type: 'get',
+            url: "/employee/employeeLogout",
+            dataType: 'json',
+            success: function (info) {
+                if (info.success) {
+                    location.href = 'login.html';
+                }
+            }
+        })
+    })
+
+})
