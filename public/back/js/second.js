60ab61ee8bdeb0447ecc2f1585774d1fe3243dc8
daxiaojiudai <937441420@qq.com>
2019-01-07 21:53:46 +0800

    product页面刚起步

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -14,7 +14,7 @@ $(function () {
                 pageSize: pageSize
             },
             success: function (info) {
-                console.log(info);
+                // console.log(info);
                 var htmlstr = template('secondTpl', info);
                 $('tbody').html(htmlstr);
                 $("#paginator").bootstrapPaginator({
@@ -31,6 +31,7 @@ $(function () {
     }
 
 
+    // 2. 点击添加按钮, 显示添加模态框
     $('#addBtn').on('click', function () {
         $('#secondModal').modal('show');
         $.ajax({
@@ -49,61 +50,92 @@ $(function () {
     });
 
 
+    // 3. 给所有的下拉菜单的 a 添加点击事件 (通过事件委托)
     $('.dropdown-menu').on('click', 'a', function () {
         var txt = $(this).text();
         $('#dropdownText').text(txt);
+        var id = $(this).data('id');
+        $('[name="categoryId"]').val(id);
+        $('#form').data("bootstrapValidator").updateStatus("categoryId", "VALID");
+
     })
 
 
+    // 4. 配置fileupload进行初始化
     $("#fileupload").fileupload({
         dataType: "json",
         done: function (e, data) {
-            console.log(data);
+            // console.log(data);
             var picURL = data.result.picAddr;
             $('#imgBox img').attr('src', picURL);
+            $('[name="brandLogo"]').val(picURL);
+            $('#form').data("bootstrapValidator").updateStatus("brandLogo", "VALID");
         }
     });
 
-    // $('#submitBtn').on('click', function () {
-    //     $("#form").bootstrapValidator({
-    //         feedbackIcons: {
-    //             valid: 'glyphicon glyphicon-ok',
-    //             invalid: 'glyphicon glyphicon-remove',
-    //             validating: 'glyphicon glyphicon-refresh'
-    //         },
-    //         fields: {
-    //             categoryName: {
-    //                 validators: {
-    //                     notEmpty: {
-    //                         message: "请输入二级分类名称"
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     });
-    
-    //     $("#form").on('success.form.bv', function (e) {
-    //         e.preventDefault();
-    //         $.ajax({
-    //             type: 'post',
-    //             url: '/category/addSecondCategory',
-    //             dataType: 'json',
-    //             data: $('#form').serialize(),
-    //             success: function (info) {
-    //                 console.log(info);
-    //                 if (info.success) {
-    //                     $('#secondModal').modal('hide');
-    //                     currentPage = 1;
-    //                     render();
-    //                     // 重置表单
-    //                     $('#form').data("bootstrapValidator").resetForm(true);
-    //                 }
-    //             }
-    //         })
-    //     });
-    // })
 
-    
+
+    // 5. 添加表单校验
+    $('#form').bootstrapValidator({
+        // 配置不校验的类型, 对 hidden 需要进行校验
+        excluded: [],
+
+        // 配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',    // 校验成功
+            invalid: 'glyphicon glyphicon-remove',   // 校验失败
+            validating: 'glyphicon glyphicon-refresh'  // 校验中
+        },
+        fields: {
+            categoryId: {
+                validators: {
+                    notEmpty: {
+                        message: "请选择一级分类"
+                    }
+                }
+            },
+            brandName: {
+                validators: {
+                    notEmpty: {
+                        message: "请输入二级分类名称"
+                    }
+                }
+            },
+            brandLogo: {
+                validators: {
+                    notEmpty: {
+                        message: "请上传图片"
+                    }
+                }
+            },
+        }
+    });
+
+
+
+    // 6. 注册表单校验成功事件, 阻止默认的提交, 通过ajax提交
+    $('#form').on('success.form.bv', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "/category/addSecondCategory",
+            dataType: 'json',
+            data: $('#form').serialize(),
+            success: function (info) {
+                // console.log(info);
+                if (info.success) {
+                    $('#secondModal').modal('hide');
+                    currentPage = 1;
+                    render();
+                    $('#form').data('bootstrapValidator').resetForm(true);
+                    $('#dropdownText').text('请选择一级分类');
+                    $('#imgBox img').attr('src', "./images/none.png")
+                }
+            }
+        })
+    })
+
+
 
 
 
